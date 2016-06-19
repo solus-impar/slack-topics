c638db48a28568eff1397d05ed7e6c813ab27c9b
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2016-06-19 14:05:03 -0700

    Create requirements.txt and update setup.py.
    Explicitly list program requirements in requirements.txt and avoid
    mixing package installation and system requirements by removing crontab
    dependency in setup.py. If the user wants to create a crontab entry they
    could do it themselves post-installation.

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
-from crontab import CronTab
-import os
+import os.path
+from setuptools import setup
+from pip.req import parse_requirements
 
-cmd = os.popen("which python3").read().strip("\n")
-pwd = os.popen("pwd").read().strip("\n")
-cron = CronTab(user = True)
-job = cron.new(command = cmd + " " + pwd + "/tb2k.py")
-job.day.on(6)
-job.enable()
-cron.write()
+here = os.path.abspath(os.path.dirname(__file__))
+
+requirements_path = os.path.join(here, 'requirements.txt')
+install_requirements = parse_requirements(requirements_path, session=False)
+requirements = [str(ir.req) for ir in install_requirements]
+
+setup(
+    name='tb2k',
+    version='0.1.0',
+    description='A python3 slack topic bot.',
+    author='Michael Canoy',
+    author_email='canoym@students.wwu.edu',
+    url='https://gitlab.cs.wwu.edu/canoym/tb2k',
+    install_requires=requirements,
+    license='MIT',
+    keywords='topic bot Slack',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tb2k=tb2k:main',
+        ]
+    },
+)
