cf040d9bd8a69b74e992864f3d7c43dded75d776
Mike Canoy <canoym@wwu.edu>
2017-10-16 12:36:19 -0700

    Add bs4 util

--- slack_topics/topics/utils.py
+++ slack_topics/topics/utils.py
@@ -1,20 +1,8 @@
+"""slack-topics: utilities for fetching topics."""
 import re
 import sys
 import requests
-
-
-def topic(c):
-    """Identify a callable as a topic."""
-    c.__topic__ = " ".join(w.title() for w in uncamel(c.__name__).split('_'))
-    return c() if type(c) is type else c
-
-
-def uncamel(s):
-    """
-    Convert CamelCase class names into lower_snake_case.
-    Taken from http://stackoverflow.com/a/1176023/3288364
-    """
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s).lower()
+from bs4 import BeautifulSoup
 
 
 def fetch_json(url):
@@ -32,3 +20,23 @@ def fetch_json(url):
             sys.exit("tb2k: error: invalid JSON at {}".format(url))
     else:
         sys.exit("tb2k: error: {} fetching {}".format(status_code, url))
+
+
+def topic(c):
+    """Identify a callable as a topic."""
+    c.__topic__ = " ".join(w.title() for w in uncamel(c.__name__).split('_'))
+    return c() if type(c) is type else c
+
+
+def uncamel(s):
+    """
+    Convert CamelCase class names into lower_snake_case.
+    Taken from http://stackoverflow.com/a/1176023/3288364
+    """
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s).lower()
+
+
+def url_to_soup(url):
+    """url -> soup"""
+    html = requests.get(url)
+    return BeautifulSoup(html.text, 'html.parser')
