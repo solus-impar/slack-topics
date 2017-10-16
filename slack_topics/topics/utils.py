bea9aa53609d5c0e9212a852d3e63b3f2360d60e
Mike Canoy <canoym@wwu.edu>
2017-10-16 15:11:47 -0700

    Fix error messages w/ old name

--- slack_topics/topics/utils.py
+++ slack_topics/topics/utils.py
@@ -7,19 +7,27 @@ from bs4 import BeautifulSoup
 
 def fetch_json(url):
     """Fetch data from a URL and attempt to parse it as JSON."""
+    error = "slack-topics: {}"
+
     try:
         response = requests.get(url)
     except requests.ConnectionError:
-        sys.exit("tb2k: error: cannot connect to {}".format(url))
+        sys.exit(error.format(
+            "Cannot connect to {}".format(url)
+        ))
 
     status_code = response.status_code
     if status_code == 200:
         try:
             return response.json()
         except ValueError:
-            sys.exit("tb2k: error: invalid JSON at {}".format(url))
+            sys.exit(error.format(
+                "Invalid JSON at {}".format(url)
+            ))
     else:
-        sys.exit("tb2k: error: {} fetching {}".format(status_code, url))
+        sys.exit(error.format(
+            "{} fetching {}".format(status_code, url)
+        ))
 
 
 def topic(c):
